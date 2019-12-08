ef40504d5d2e70403159a8acc3f21c99be27e50a
Fidel <habtezion@gmail.com>
2019-12-07 18:03:05 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,41 @@
 // BEGIN: Global variables
-const randomUserUrl = 'https://randomuser.me/api/?results=12&nat=us';
-const galleryDiv = document.getElementById('gallery');
+const randomUserUrl = "https://randomuser.me/api/?results=12&nat=us";
+const galleryDiv = document.getElementById("gallery");
 // END: Global variables
 
-
 // BEGIN: Handle all fetch requests
-const randomUsers = fetchRequest(randomUserUrl).then(randomUsers => {
-    console.log(randomUsers.results[3]);
-    console.log(Object.keys(randomUsers.results[3]));
-    console.log(Object.keys(randomUsers.results));
-    console.log(randomUsers.results);
-    generateGalleryUI(randomUsers.results);
+let randomUsersObject = {};
+let randomUserArray = [];
+const randomUsers = fetchRequest(randomUserUrl).then(randomUser => {
+  //   console.log(randomUser.results[3]);
+  //   console.log(Object.keys(randomUser.results[3]));
+  //   console.log(Object.keys(randomUser.results));
+  //   console.log(typeof randomUser.results);
+  //   randomUsersObject = randomUsersObject.push(randomUser.results);
+  //   randomUsersObject = randomUser.results;
+  generateGalleryUI(randomUser.results);
 });
+// const x = randomUsersObject;
 
+console.log("HERE HERE HERE");
+console.log("users", ...randomUserArray);
+console.log("HERE HERE HERE");
 // END: Handle all fetch requests
-
 
 // BEGIN: Handle markup generation
 
 function generateModalUI(randomUser) {
-    const modalDiv = document.createElement('div');
-    modalDiv.classList = 'modal-container';
-    document.body.appendChild(modalDiv);
-    let modalUI = `
+  const modalDiv = document.createElement("div");
+  modalDiv.classList = "modal-container";
+  modalDiv.setAttribute("id", randomUser.profileId);
+  document.body.appendChild(modalDiv);
+  let modalUI = `
     
         <div class="modal">
             <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
             <div class="modal-info-container">
                 <img class="modal-img" src="${randomUser.picture.medium}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${randomUser.title} ${randomUser.firsgt} ${randomUser.last}</h3>
+                <h3 id="name" class="modal-name cap">${randomUser.name.title}. ${randomUser.name.first} ${randomUser.name.last}</h3>
                 <p class="modal-text">${randomUser.email}</p>
                 <p class="modal-text cap">${randomUser.location.city}</p>
                 <hr>
@@ -38,40 +45,46 @@ function generateModalUI(randomUser) {
             </div>
         </div>
 
-        // IMPORTANT: Below is only for exceeds tasks 
+        
         <div class="modal-btn-container">
             <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
             <button type="button" id="modal-next" class="modal-next btn">Next</button>
         </div>
     </div>
     `;
-    modalDiv.innerHTML = modalUI;
-    const x_close_modal = document.getElementById('modal-close-btn');
-    x_close_modal.addEventListener('click', (e) => {
-        modalDiv.remove();
-    });
+  modalDiv.innerHTML = modalUI;
+  const x_close_modal = document.getElementById("modal-close-btn");
+  x_close_modal.addEventListener("click", e => {
+    modalDiv.remove();
+  });
 
-    // handle next / previous click events
-    const next = document.getElementById('modal-next');
-    const prev = document.getElementById('modal-prev');
-    next.addEventListener('click', () => {
-        nextCard();
-    });
-    prev.addEventListener('click', () => {
-        prevCard();
-    });
+  // handle next / previous click events
+  const next = document.getElementById("modal-next");
+  const prev = document.getElementById("modal-prev");
+
+  next.addEventListener("click", () => {
+    showCard(randomUser.profileId + 1);
+  });
+  prev.addEventListener("click", () => {
+    showCard(randomUser.profileId - 1);
+  });
 }
 
 function generateGalleryUI(randomUsers) {
-    let cardId = 0;
-    randomUsers.map(randomUser => {
-        const cardDiv = document.createElement('div');
-        cardDiv.className = 'card';
-        cardDiv.id = cardId;
-        cardId ++;
-        galleryDiv.appendChild(cardDiv);
+  console.log(typeof randomUsers);
 
-        let htmlUI = `
+  let cardId = 0;
+  randomUsers.map(randomUser => {
+    randomUserArray.push(randomUser);
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    // cardDiv.id = cardId;
+    cardDiv.setAttribute("id", cardId);
+    randomUser.profileId = cardId;
+    cardId++;
+    galleryDiv.appendChild(cardDiv);
+
+    let htmlUI = `
             <div class="card-img-container">
                 <img class="card-img" src="${randomUser.picture.thumbnail}" alt="profile picture">
             </div>
@@ -81,33 +94,38 @@ function generateGalleryUI(randomUsers) {
                 <p class="card-text cap">${randomUser.location.city}, ${randomUser.location.state}</p>
             </div>
         `;
-        cardDiv.innerHTML = htmlUI;
+    cardDiv.innerHTML = htmlUI;
 
-        cardDiv.addEventListener('click', () => {
-            generateModalUI(randomUser);
-        });
+    cardDiv.addEventListener("click", () => {
+      generateModalUI(randomUser);
     });
-
+  });
 }
 // END: Handle markup generation
 
 // BEGIN: Handle events
-function nextCard() {
-    console.log('next');
+function showCard(profileId) {
+  console.log("next" + parseInt(profileId, 10));
+  const id = parseInt(profileId, 10);
+  //   console.log(typeof id);
+
+  console.log(randomUsers.results[2]);
+
+  generateModalUI(randomUsers.results[id]);
 }
 
-function prevCard() {
-    console.log('previous');
+function prevCard(profileId) {
+  console.log("previous");
 }
 // END: Handle events
 
 // BEGIN: Helper functions
 async function fetchRequest(url) {
-    try {
-        const response = await fetch(url);
-        return await response.json();
-    } catch(e) {
-        throw e;
-    }
+  try {
+    const response = await fetch(url);
+    return await response.json();
+  } catch (e) {
+    throw e;
+  }
 }
 // END: Helper functions
